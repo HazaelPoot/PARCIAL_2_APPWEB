debef2056d012bc19f90f9bd21e8de692f17b40c
HazaelPoot <91893556+HazaelPoot@users.noreply.github.com>
2021-11-04 11:41:58 -0600

    Add files via upload

---
+++ PRACTICA2/AlcoholemiaApp/Alcoholemia.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AlcoholemiaWebApp
+{
+    public class Datos
+    {
+        public double total {get; set;}
+
+        public double cantidad {get; set;}
+        public double mililtro { get; set; }
+        public double alcohol { get; set; }
+        public double peso { get; set; }
+        public double edad { get; set; }
+    }
+}
